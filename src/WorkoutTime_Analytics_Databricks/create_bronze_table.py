af929517588fe0c1a0c4876bf277e4e11d5a1c4b
Gaurav Thagunna <adi@MACBOOK-GTHAGUNNA.local>
2026-02-03 10:15:50 +0545

    packaging started for ingestion

--- src/WorkoutTime_Analytics_Databricks/create_bronze_table.py
+++ src/WorkoutTime_Analytics_Databricks/create_bronze_table.py
@@ -1,10 +1,98 @@
-from ingestion import (
-    catalog,
-    ingestion_db,
-    ingestion_table,
-    bronze_db,
-)
-from spark import get_spark     
+import json
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType
+from pyspark.sql import SparkSession, DataFrame
 
-spark = get_spark(profile="dev-free-edition")
 
+from spark import get_spark
+
+catalog = "dev"
+
+ingestion_db = "control"
+ingestion_table = "ingest_table_registry"
+bronze_db = "bronze_workout"
+schema_registry_table = "schema_registry"
+history_table = "ingest_run_history"
+
+
+# spark = get_spark(profile="dev-free-edition")
+
+
+def get_defined_schema(table_id: int,spark: SparkSession):
+    schema = (
+        spark.table(f"{catalog}.{ingestion_db}.{schema_registry_table}")
+        .filter(f"table_id = {table_id} AND is_latest = TRUE")
+        .orderBy(F.desc("version"))
+        .limit(1)
+        .collect()[0]
+    )
+
+    return StructType.fromJson(json.loads(schema.schema_definition))
+
+
+
+
+
+def load_date(spark, configs_dict, schema) -> DataFrame:
+    """Load data from a given path with the provided schema."""
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", configs_dict["source_format"])
+        .option("cloudFiles.schemaLocation", configs_dict["checkpoint_path"] + "schema/" + configs_dict["target_table"])
+        .option("cloudFiles.schemaEvolutionMode", "rescue")
+        .option("cloudFiles.maxFilesPerTrigger", 1)
+        .schema(schema)
+        .load(configs_dict["source_path"])
+    )
+
+
+def ensure_target_table_exists(spark, configs_dict):
+    try:
+        print(f"Creating table: {configs_dict['target_table']}")
+        spark.sql(
+            f"DESCRIBE TABLE {configs_dict['target_catalog']}.{configs_dict['target_schema']}.{configs_dict['target_table']}"
+        )
+        return
+    except Exception:
+        pass
+
+    spark.sql(
+        f"CREATE TABLE {configs_dict['target_catalog']}.{configs_dict['target_schema']}.{configs_dict['target_table']}"
+    )
+
+
+def merge_to_delta(spark: SparkSession, df: DataFrame, configs_dict):
+    ensure_target_table_exists(spark, configs_dict)
+    print(f"merge keys : {configs_dict['merge_key']}")
+
+    target_full_name = (
+        f"{configs_dict['target_catalog']}.{configs_dict['target_schema']}.{configs_dict['target_table']}"
+    )
+
+    if not configs_dict["merge_key"]:
+        print("merge keys not present")
+        return (
+            df.write.format("delta")
+            .option("mergeSchema", "true")
+            .mode(configs_dict["write_mode"])
+            .saveAsTable(target_full_name)
+        )
+
+    else:
+        df.createOrReplaceTempView("source_table")
+
+        condition = " AND ".join([f"t.{key} = s.{key}" for key in configs_dict["merge_key"]])
+
+        query = f"""
+        MERGE INTO {target_full_name} AS t
+        USING source_table AS s
+        ON {condition}
+        WHEN MATCHED THEN
+        UPDATE SET * 
+        WHEN NOT MATCHED THEN
+        INSERT * 
+            
+        """
+
+        spark.sql(query)
--- src/WorkoutTime_Analytics_Databricks/ingestion.py
+++ src/WorkoutTime_Analytics_Databricks/ingestion.py
@@ -1,3 +1,11 @@
+"""
+1. setup_ingestion_master.py - Sets up the ingestion master table and inserts a test record.
+2. schema_registry_table.py - Creates a schema registry entry based on the source data schema.
+3. create_bronze_table.py - Creates bronze tables based on ingestion configurations and schema regisrty configuration.
+"""
+
+
+
 catalog = "dev"
 
 ingestion_db = "control"
@@ -12,11 +20,10 @@ table_id = 1
 
 from spark import get_spark
 
-spark = get_spark(profile="dev-free-edition")
-print("Spark session initialized:", spark)
 
 
-def get_configs(table_id: int):
+
+def get_configs(table_id: int,spark):
     query = f"""
     SELECT *
     FROM {catalog}.{ingestion_db}.{ingestion_table}
@@ -52,5 +59,48 @@ def get_configs(table_id: int):
     return configs_dict
 
 
-configs_dict = get_configs(table_id=table_id)
-print("Ingestion configurations retrieved:", configs_dict)
+
+
+
+from create_bronze_table import get_defined_schema, load_date, merge_to_delta
+
+from pyspark.sql import SparkSession, DataFrame
+
+
+def start_stream(df:DataFrame, configs_dict:dict,spark:SparkSession):
+    writer = (
+        df.writeStream
+          .foreachBatch(lambda batch_df, batch_id:
+                        merge_to_delta(spark, batch_df, configs_dict))
+         .option("checkpointLocation", configs_dict["checkpoint_path"]+configs_dict["target_table"]) 
+          .queryName(configs_dict["target_table"])
+    )
+
+    if configs_dict["trigger_mode"] == "availableNow":
+        return writer.trigger(availableNow=True).start()
+    else:
+        return writer.trigger(processingTime=configs_dict["processing_time"]).start()
+
+
+
+
+def main():
+
+    spark = get_spark(profile="dev-free-edition")
+    print("Spark session initialized:", spark)
+
+    configs_dict = get_configs(table_id=table_id,spark=spark)
+    print("Ingestion configurations retrieved:", configs_dict)
+
+
+    schema = get_defined_schema(table_id=table_id,spark=spark)
+    print(f"Schema retrieved for table_id: {table_id} \n {schema}")
+
+    raw_data = load_date(spark=spark,configs_dict=configs_dict,schema=schema)
+
+    start_stream(df=raw_data, configs_dict=configs_dict,spark=spark)
+
+
+if __name__ == "__main__":
+    main()
+
